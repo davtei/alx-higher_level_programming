bb40228f1e9fd1513528147d5f3ed9a5649c2e60
David Tei <daveteidt@gmail.ocm>
2023-03-08 19:04:47 -0800

    Added print_comb3

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for tens in range(0, 10):
+    for ones in range(tens + 1, 10):
+        if tens == 8 and ones == 9:
+            print("{}{}".format(tens, ones))
+        else:
+            print("{}{}".format(tens, ones), end=", ")
