c9eb7a302631882db33db97a8d9477b079373102
David Tei <daveteidt@gmail.ocm>
2023-03-09 04:16:28 -0800

    Added uppercase function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    """Function that prints a string in uppercase followed by a new line"""
+    """Function that prints a string in uppercase."""
     for c in str:
         if ord(c) >= 97 and ord(c) <= 122:
             c = chr(ord(c) - 32)
