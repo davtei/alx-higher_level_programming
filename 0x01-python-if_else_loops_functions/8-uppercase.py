eb1000cd49954adc4fa1f15438bc507957a02557
David Tei <daveteidt@gmail.ocm>
2023-03-09 07:00:18 -0800

    Added uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    """Function that prints a string in uppercase."""
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            c = chr(ord(c) - 32)
-        print("{:s}".format(c), end="")
-    print("")
