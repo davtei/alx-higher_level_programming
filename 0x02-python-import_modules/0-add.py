117c03c93b4de8b03839a8900953e07b95937ed7
David Tei <daveteidt@gmail.ocm>
2023-03-10 23:11:25 -0800

    Added add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """Program that imports add form add_0"""
     from add_0 import add
+
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add(a, b)))
