7069315ac2d0d092192664a384851987eb7c0319
David Tei <daveteidt@gmail.ocm>
2023-03-10 20:32:29 -0800

    Added hidden_discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+import hidden_4
+def hidn():
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
+
+if __name__ == "__main__":
+    hidn()
