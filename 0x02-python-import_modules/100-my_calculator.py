5b60f33f3d1f692b0f2f31094d5ee765a266de93
David Tei <daveteidt@gmail.ocm>
2023-03-10 22:38:41 -0800

    Added my_calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,7 +2,7 @@
 
 if __name__ == "__main__":
     from sys import argv
-    if argv != 4:
+    if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         quit(1)
     a = int(argv[1])
