89b55b0f2b7ae5fb6f9112f865ecf86f62d77bb9
David Tei <daveteidt@gmail.ocm>
2023-03-13 17:13:22 -0700

    Added new_in_list

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if 0 <= idx < len(my_list):
+        new_list = my_list[:]
+        new_list[idx] = element
+        return(new_list)
+    else:
+        return(my_list)
