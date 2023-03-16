30059a793fe2f9408c2e1d81325759129712a42f
David Tei <daveteidt@gmail.ocm>
2023-03-16 09:24:14 -0700

    weight_average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    if not my_list:
+        return(0)
+
+    weightd = 0
+    dp = 0
+    for tupl in my_list:
+        weightd += tupl[0] * tupl[1]
+        dp += tupl[1]
+    return (weightd / dp)
