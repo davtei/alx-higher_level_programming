fe7e274d5d65bb408c00d174e8a68478a6a299e9
David Tei <daveteidt@gmail.ocm>
2023-03-28 23:09:14 -0700

    Added 1-square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""An empty class that defines a square"""
+
+class Square:
+    """Class for a square"""
+    
+    def __init__(self, size):
+        """Initializes square with private instance attribute, size
+        Args:
+            size (int): size of the square.
+        """
+        self.__size = size
