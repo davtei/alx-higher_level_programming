08b4ce54197f58dbf0b1ad8ddd73dbf958de677a
David Tei <daveteidt@gmail.ocm>
2023-04-11 23:52:21 -0700

    Added is_same_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,8 @@
+#!/usr/bin/python3
+"""Module containing is_same_class function."""
 
+
+def is_same_class(obj, a_class):
+    """function that returns True if the object is exactly
+    an instance of the specified class ; otherwise False."""
+    return(type(obj) == a_class)
