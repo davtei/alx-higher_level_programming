0f3c5ec80ccd796165d144fe580896eaea3eda40
David Tei <daveteidt@gmail.ocm>
2023-04-11 23:48:14 -0700

    Added my_list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+"""The list class"""
 
+
+class MyList(list):
+    """A class that inherits from list."""
+    def __init__(self):
+        """Initialize objects created from MyList."""
+        super().__init__()
+
+    def print_sorted(self):
+        """Public instance method that prints the list
+        in ascending order (sorted)."""
+        print(sorted(self))
