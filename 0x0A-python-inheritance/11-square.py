8b3a8735a6d7b9f930e38a05a319a1382ce961c8
David Tei <daveteidt@gmail.ocm>
2023-04-12 02:38:23 -0700

    Added 11-square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Class Square, a subclass of BaseGeometry and Rectangle class."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class Square that inherits from the subclass Rectangle."""
+    def __init__(self, size):
+        """Instantiation with size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of a square."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Defines the print() representation of a
+        description of the rectangle."""
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
