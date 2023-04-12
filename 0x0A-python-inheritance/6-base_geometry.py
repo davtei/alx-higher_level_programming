2bdbcd015182eb2d6d6985a55994e68ec8684a3e
David Tei <daveteidt@gmail.ocm>
2023-04-12 00:24:52 -0700

    Added 6-base_geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class BaseGeometry"""
+
+
+class BaseGeometry():
+    """A class BaseGeometry that has a public instance method area()."""
+    def area(self):
+        """area() raises an Exception."""
+        raise Exception("area() is not implemented")
