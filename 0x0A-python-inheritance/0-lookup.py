68265e51db5835547cf7354838d62aa4e52adfe8
David Tei <daveteidt@gmail.ocm>
2023-04-11 22:21:13 -0700

    Added lookup

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module containing the lookup function"""
+
+
+def lookup(obj):
+    """Function that returns the list of available
+    attributes and methods of an object"""
+
+    return dir(obj)
