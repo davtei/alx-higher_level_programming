c6f8dac6f182f83a9f81c766a85621b55ca71f05
David Tei <daveteidt@gmail.ocm>
2023-04-12 17:00:19 -0700

    Added student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A Student class."""
+
+class Student():
+    """A class that defines a student."""
+    def __init__(self, first_name, last_name, age):
+        """Initialize with public instance attributes first_name,
+        last_name and age."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Public method that retrieves a dictionary representation
+        of a Student instance (same as 8-class_to_json.py)."""
+        return self.__dict__
