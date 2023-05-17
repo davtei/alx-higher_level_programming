b0d6315a56467ef1872aded559a3075220d28ded
David Tei <daveteidt@gmail.com>
2023-05-17 05:37:44 +0000

    Added top_score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- A script that lists all records of the table second_table of the database hbtn_0c_0
+-- Score and name are displayed
+-- Records are ordered in descending order based on score
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
