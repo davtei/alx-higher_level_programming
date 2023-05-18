4895036fb6d3a1f0a807b84b202616a92fcaaf4a
David Tei <daveteidt@gmail.com>
2023-05-18 22:46:48 +0000

    Modded count_shows_by_genre script

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -5,8 +5,7 @@
 --Results are sorted in descending order by the number of shows linked
 -- Uses only one SELECT statement
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-    FROM tv_genres
-        RIGHT JOIN tv_show_genres
+    FROM tv_genres RIGHT JOIN tv_show_genres
         ON tv_genres.id = tv_show_genres.genre_id
-    GROUP BY genre.name
+    GROUP BY genre
     ORDER BY number_of_shows DESC;
