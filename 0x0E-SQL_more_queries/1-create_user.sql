3bd75e564b054f8a719504d68b95bf4bac22019d
David Tei <daveteidt@gmail.com>
2023-05-18 05:10:40 +0000

    Modded create_user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,4 +3,4 @@
 -- user_0d_1 password is set to user_0d_1_pwd
 -- The script does not fail if user_0d_1 already exitst
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT *.* TO user_0d_1@localhost
+GRANT ALL ON *.* TO user_0d_1@localhost
