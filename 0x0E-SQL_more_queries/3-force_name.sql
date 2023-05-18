68140179b67d580a571448cb87390c5be18a0761
David Tei <daveteidt@gmail.com>
2023-05-18 05:22:37 +0000

    Added force_name script

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- A script that creates the table force_name on the MySQL server.
+-- The script does not fail if force_name already exitst
+CREATE TABLE IF NOT EXISTS `force_name` (
+    `id` INT
+    `name` VARCHAR(256) NOT NULL
+);
