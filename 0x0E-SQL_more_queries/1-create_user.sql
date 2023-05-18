d5cd432f97190ae68f627af8ff417d1d17254ed9
David Tei <daveteidt@gmail.com>
2023-05-18 05:07:15 +0000

    Added create_user

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- A script that creates the MySQL server user user_0d_1
+-- user_0d_1 has all privileges on the MySQL server
+-- user_0d_1 password is set to user_0d_1_pwd
+-- The script does not fail if user_0d_1 already exitst
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
+GRANT *.* TO user_0d_1 @localhost
