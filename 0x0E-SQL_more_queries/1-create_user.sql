c1ea8f3dd26ce87374f5f36aad8067f40240ca50
David Tei <daveteidt@gmail.com>
2023-05-18 05:09:25 +0000

    Modded create_user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,5 +2,5 @@
 -- user_0d_1 has all privileges on the MySQL server
 -- user_0d_1 password is set to user_0d_1_pwd
 -- The script does not fail if user_0d_1 already exitst
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY user_0d_1_pwd;
-GRANT *.* TO user_0d_1 @localhost
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT *.* TO user_0d_1@localhost
