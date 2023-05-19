27603d52ce109638c5fdfa153b29d1887b437a09
David Tei <daveteidt@gmail.com>
2023-05-19 00:15:37 +0000

    Modded shows_by_genre script

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- A script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 -- display NULL in the genre column if a show doesn’t have a genre
--- aach record displays: tv_shows.title - tv_genres.name
+-- Each record displays: tv_shows.title - tv_genres.name
 -- Results are sorted in ascending order by the show title and genre name
 -- Uses only one SELECT statement
 SELECT title, name
