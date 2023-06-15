f75fc9b8f04836aef37a74a56f1c7f983be2bf33
David Tei <daveteidt@gmail.com>
2023-06-15 06:35:07 +0000

    Added multi_c script

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  const num = Number(process.argv[2]);
+  let i = 0;
+  while(i < num) {
+    console.log('C is fun')
+    i++;
+  }
+}
