1ee5168329d489a193d68d168151459d01ca81f9
David Tei <daveteidt@gmail.com>
2023-06-15 05:10:16 +0000

    Modified arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.length === 2) {
   console.log('No Argument');
 } else if (process.argv.length === 3) {
-  console.log('Argument Found');
+  console.log('Argument found');
 } else {
-  console.log('Arguments Found');
+  console.log('Arguments found');
 }
