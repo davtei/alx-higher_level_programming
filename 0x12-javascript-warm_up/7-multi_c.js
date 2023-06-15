6e459b5f08ccce071f0b3656e40c521d8563f1f7
David Tei <daveteidt@gmail.com>
2023-06-15 06:37:02 +0000

    Modified multi_c script

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,8 +4,8 @@ if (process.argv[2] === undefined || isNaN(process.argv[2])) {
 } else {
   const num = Number(process.argv[2]);
   let i = 0;
-  while(i < num) {
-    console.log('C is fun')
+  while (i < num) {
+    console.log('C is fun');
     i++;
   }
 }
