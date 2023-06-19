13de110166cd51558472a5e64b3b0b4e3e454add
David Tei <daveteidt@gmail.com>
2023-06-19 05:32:35 +0000

    Added model_city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""The City class."""
+
+from model_state import Base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Integer, String, MetaData, Column
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+
+class City(Base):
+    """A City class that has the attributes id, name and state_id."""
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
