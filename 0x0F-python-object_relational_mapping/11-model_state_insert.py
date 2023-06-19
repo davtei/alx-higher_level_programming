39c5892970e85f153957e3d5edc8db526fa476af
David Tei <daveteidt@gmail.com>
2023-06-19 05:10:58 +0000

    Added model_state_insert script

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 
     new_state = State(name='Louisiana')
     session.add(new_state)
-    
+
     new_instance = session.query(State).filter_by(name='Louisiana').first()
     print(new_instance.id)
     session.commit()
