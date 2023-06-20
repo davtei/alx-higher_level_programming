0dfe955a48c32f58e4c46203f451e073e2677951
David Tei <daveteidt@gmail.com>
2023-06-20 11:49:48 +0000

    Modified model_city script

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """The City class."""
-
 from model_state import Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Integer, String, MetaData, Column
