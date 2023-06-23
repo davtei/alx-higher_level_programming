43fe990907c43fe5c5011575e7c4380f0453f014
David Tei <daveteidt@gmail.com>
2023-06-23 16:04:44 +0000

    Added square 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const SquareC = require('./5-square');
+
+class Square extends SquareC {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      let sq = '';
+      for (let j = 0; j < this.width; j++) {
+        sq += c;
+      }
+      console.log(sq)
+    }
+  }
+}
+
+module.exports = Square;
