14669fbc8eb8ac7b92bea8f37c45b998d83d72c8
David Tei <daveteidt@gmail.com>
2023-07-01 05:30:23 +0000

    Added post_email script

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A Python script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter, and finally displays
+the body of the response.
+Usage: ./6-post_email.py <URL> <e-mail>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    r = requests.post(url, data=value)
+    print(r.text)
