015bc90b945304df3bcc8945f6f92d92a940082b
David Tei <daveteidt@gmail.com>
2023-08-09 05:36:54 +0000

    A subs script

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A Python script that queries the Reddit API using request module."""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """A function that queries the Reddit API and returns the number of
+    subscribers (not active users, total subscribers) for a given subreddit.
+    If an invalid subreddit is given, the function should return 0."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
